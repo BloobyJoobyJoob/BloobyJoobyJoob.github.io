386e764b7b698f1067722aa168e74a5fbde07701
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2023-09-11 21:36:14 +1200

    Add blog for js and the DOM

---
+++ example-code.js
@@ -0,0 +1,28 @@
+for (let i = 0; i < array.length; i++) {
+  console.log[i]
+}
+
+array.forEach((element) => {
+  console.log(element)
+})
+
+let i = 0
+while (i < array.length) {
+  i++
+  console.log(array[i])
+}
+
+let element
+element = document.getElementById('box')
+
+const carData = {
+  type: 'Fiat',
+  model: '500',
+  color: 'white',
+}
+
+let carColor = carData.color
+
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' /*etc......*/]
+
+let letter = alphabet[5] // Get the letter at the 5 position in the array (e)
